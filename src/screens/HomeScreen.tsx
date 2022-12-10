e2bd03d665e0cd546a33e23297f51152cbdeafb3
yanwhocodes <ynawhocodes@gmail.com>
2022-12-10 23:16:24 +0900

    [remove] #2 사용하지 않는 dummyData 코드 삭제

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,21 +3,6 @@ import {View, Text, StyleSheet} from 'react-native';
 import {Container} from '../components/Container';
 import {LabelCardContainer} from '../components/LabelCardContainer';
 
-const dummyData = [
-  {
-    label: '오늘의 질문',
-    content:
-      '다시 돌아가도 그만큼은 못하겠다!\n가장 열정을 쏟은 순간은 언제인가요?',
-    cardBgColor: '#EE3E36',
-    cardHeight: '50%',
-  },
-  {
-    label: 'HOT 질문',
-    content: '순간이 기억 남는 노래가 있나요?',
-    cardBgColor: '#F6D500',
-    cardHeight: '25%',
-  },
-];
 const HomeScreen = ({navigation}: any) => {
   return (
     <Container>
